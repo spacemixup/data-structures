fc6e890ed9cade3ae340d39a23525a691654191d
spacemixup <xchrislee@gmail.com>
2015-05-13 12:10:19 -0700

    Passed first 3 stack tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,20 @@
 var Stack = function(){
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+  size++
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+  // return 0;
+  return size;
   };
 
   return someInstance;
