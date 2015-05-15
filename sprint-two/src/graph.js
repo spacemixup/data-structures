aa117d23f20c840c8d1d56f5e4f5064344764091
spacemixup <xchrislee@gmail.com>
2015-05-15 12:14:57 -0700

    Graph function works through setEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,38 @@
 
 
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = GraphNode(node);
 };
 
 Graph.prototype.contains = function(node){
+  for (var key in this.nodes) {
+    if (this.nodes[key].value = node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
-};
+  delete this.nodes[node];
+  };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  for (var i = 0; i < this.nodes[fromNode].connections.length; i++) {
+    if (this.nodes[fromNode].connections[i].value === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode].connections.push(this.nodes[toNode]);
+  this.nodes[toNode].connections.push(this.nodes[fromNode]);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
@@ -28,5 +45,11 @@ Graph.prototype.forEachNode = function(cb){
  * Complexity: What is the time complexity of the above functions?
  */
 
+var GraphNode = function(value) {
+  var newNode = {};
+  newNode.value = value;
+  newNode.connections = [];
+  return newNode;
+};
 
 
