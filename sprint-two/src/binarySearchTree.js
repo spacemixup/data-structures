ba2961840e530cc0d0d20419d26ed5c3c1959a80
spacemixup <xchrislee@gmail.com>
2015-05-16 12:06:07 -0700

    added insert functionality to binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,8 +1,8 @@
 var BinarySearchTree = function(value){
   var obj = {};
   obj.value = value;
-  obj.left = {};
-  obj.right = {};
+  obj.left = null;
+  obj.right = null;
 
   _.extend(obj, binaryMethods);
 
@@ -13,10 +13,25 @@ var BinarySearchTree = function(value){
 var binaryMethods = {};
 
 binaryMethods.insert = function(value) {
-  if (this.left === undefined && this.right === undefined) {
-    var node = BinarySearchTree(value);
-  } else {
+  // if (this.left === undefined && this.right === undefined) {
+  //   var node = BinarySearchTree(value);
+  //   node.first = true;
+  // }
 
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
   }
 };
 
