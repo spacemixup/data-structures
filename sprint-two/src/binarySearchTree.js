7cab2faca723164d879f2ddc8ad2f944125fcd7b
spacemixup <xchrislee@gmail.com>
2015-05-16 11:41:26 -0700

    Started BinaryTree function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,32 @@
 var BinarySearchTree = function(value){
+  var obj = {};
+  obj.value = value;
+  obj.left = {};
+  obj.right = {};
+
+  _.extend(obj, binaryMethods);
+
+  return obj
 };
 
+
+var binaryMethods = {};
+
+binaryMethods.insert = function(value) {
+  if (this.left === undefined && this.right === undefined) {
+    var node = BinarySearchTree(value);
+  } else {
+
+  }
+};
+
+binaryMethods.contains = function() {
+
+};
+
+binaryMethods.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
